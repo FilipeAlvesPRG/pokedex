a9f536912805e6c6fd8d6511843e487953b5359f
Filipe Alves Braga Rosa <54287231+FilipeAlvesPRG@users.noreply.github.com>
2023-02-14 11:59:29 -0300

    Add select component
    not functional

--- src/views/HomePage/HomePage.vue
+++ src/views/HomePage/HomePage.vue
@@ -17,6 +17,28 @@ export default {
             filteredPokemons: [],
             search: '',
             showClearButton: false,
+            types: [
+                { name: 'normal', color: '#A8A878' },
+                { name: 'fighting', color: '#C03028' },
+                { name: 'flying', color: '#A890F0' },
+                { name: 'poison', color: '#A040A0' },
+                { name: 'ground', color: '#E0C068' },
+                { name: 'rock', color: '#B8A038' },
+                { name: 'bug', color: '#A8B820' },
+                { name: 'ghost', color: '#705898' },
+                { name: 'steel', color: '#B8B8D0' },
+                { name: 'fire', color: '#F08030' },
+                { name: 'water', color: '#6890F0' },
+                { name: 'grass', color: '#78C850' },
+                { name: 'electric', color: '#F8D030' },
+                { name: 'psychic', color: '#F85888' },
+                { name: 'ice', color: '#98D8D8' },
+                { name: 'dragon', color: '#7038F8' },
+                { name: 'dark', color: '#705848' },
+                { name: 'fairy', color: '#EE99AC' },
+                { name: 'unknown', color: '#68A090' },
+                { name: 'shadow', color: '#493963' },
+            ]
         }
     },
     created: function () {
@@ -31,6 +53,9 @@ export default {
             })
     },
     methods: {
+        selectType: function () {
+            this.filteredPokemons = this.pokemons.filter((pokemon) => pokemon.type.includes(this.type));
+        },
         searchPokemons: function () {
             this.filteredPokemons = this.pokemons;
             if (this.search.trim().toLowerCase() === '') {
@@ -41,6 +66,7 @@ export default {
                 this.showClearButton = true;
             }
         },
+
         clearSearch: function () {
             this.search = '';
             this.filteredPokemons = this.pokemons;
@@ -49,8 +75,6 @@ export default {
     }
 }
 </script>
-
-
 
 <style lang="scss" src="./style.scss" scoped/>
 
@@ -66,6 +90,10 @@ export default {
         <font-awesome-icon class="icon-button" icon="fa-solid fa-magnifying-glass" />
         Buscar
     </button>
+
+    <select class="select-type">
+        <option v-for="type in types" :value="type.name" :style="{ backgroundColor: type.color }">{{ type.name }}</option>
+    </select>
 
     <div class="container-row">
         <div v-for="(pokemon, index) in filteredPokemons">
